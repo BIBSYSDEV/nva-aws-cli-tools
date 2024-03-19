f468f224b21aa1b89d26acd955553f9169621c2a
Torbjørn Kvåle <torbjokv@users.noreply.github.com>
2024-03-19 15:22:44 +0100

    Update list_old_function_versions.py with paging

--- list_old_function_versions.py
+++ list_old_function_versions.py
@@ -1,27 +1,27 @@
-from __future__ import absolute_import, print_function, unicode_literals
 import boto3, argparse
 
-def list_or_delete_old_lambda_versions(delete):
-    client = boto3.client('lambda')
-    functions = client.list_functions()['Functions']
-    for function in functions:
-        versions = client.list_versions_by_function(FunctionName=function['FunctionArn'])['Versions']
-        aliases = client.list_aliases(FunctionName=function['FunctionArn'])
-        alias_versions = [alias['FunctionVersion'] for alias in aliases['Aliases']]
-        for version in versions:
-            if version['Version'] != function['Version'] and not version['Version'] in alias_versions:
-                arn = version['FunctionArn']
-                if delete:
-                    client.delete_function(FunctionName=arn)
-                    print('Deleted {}'.format(arn))
-                else:
-                    print(arn)
+def clean_old_lambda_versions(client, delete):
+    functions_paginator = client.get_paginator('list_functions')
+    version_paginator = client.get_paginator('list_versions_by_function')
 
+    for function_page in functions_paginator.paginate():
+        for function in function_page['Functions']:
+                aliases = client.list_aliases(FunctionName=function['FunctionArn'])
+                alias_versions = [alias['FunctionVersion'] for alias in aliases['Aliases']]
+                for version_page in version_paginator.paginate(FunctionName=function['FunctionArn']):
+                        for version in version_page['Versions']:
+                                arn = version['FunctionArn']
+                                if version['Version'] != function['Version'] and version['Version'] not in alias_versions:
+                                        print('  🥊 {}'.format(arn))
+                                        if delete:
+                                            client.delete_function(FunctionName=arn)
+                                else:
+                                        print('  💚 {}'.format(arn))
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-d", "--delete", action='store_true', help="delete old versions")
     args = argParser.parse_args()
 
-    list_or_delete_old_lambda_versions(args.delete)
-
+    client = boto3.client('lambda', region_name='eu-west-1')
+    clean_old_lambda_versions(client, args.delete)
