5b98a239908d17dc0b82ab384941b26087837190
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-01-06 15:16:14 +0100

    added cli for removing old lambda versions

--- cli.py
+++ cli.py
@@ -5,6 +5,7 @@ from commands.cognito import cognito
 from commands.handle import handle
 from commands.users import users
 from commands.customers import customers
+from commands.awslambda import awslambda
 
 @click.group()
 def cli():
@@ -14,6 +15,7 @@ cli.add_command(cognito)
 cli.add_command(handle)
 cli.add_command(users)
 cli.add_command(customers)
+cli.add_command(awslambda)
 
 if __name__ == "__main__":
     cli()
---
+++ commands/awslambda.py
@@ -0,0 +1,33 @@
+import click
+import boto3
+
+@click.group()
+def awslambda():
+    pass
+
+@awslambda.command(help="Clean old versions of AWS Lambda functions.")
+@click.option('--profile', envvar='AWS_PROFILE', default='default', help='The AWS profile to use.')
+@click.option('--delete', is_flag=True, help='Delete old versions.')
+def clean_old_versions(profile, delete):
+    session = boto3.Session(profile_name=profile)
+    client = session.client('lambda')
+
+    _clean_old_lambda_versions(client, delete)
+
+def _clean_old_lambda_versions(client, delete):
+    functions_paginator = client.get_paginator('list_functions')
+    version_paginator = client.get_paginator('list_versions_by_function')
+
+    for function_page in functions_paginator.paginate():
+        for function in function_page['Functions']:
+            aliases = client.list_aliases(FunctionName=function['FunctionArn'])
+            alias_versions = [alias['FunctionVersion'] for alias in aliases['Aliases']]
+            for version_page in version_paginator.paginate(FunctionName=function['FunctionArn']):
+                for version in version_page['Versions']:
+                    arn = version['FunctionArn']
+                    if version['Version'] != function['Version'] and version['Version'] not in alias_versions:
+                        print('  🥊 {}'.format(arn))
+                        if delete:
+                            client.delete_function(FunctionName=arn)
+                    else:
+                        print('  💚 {}'.format(arn))
