4ff5ecc11d841df9807de77f3b0ce439612954eb
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-01-07 15:55:13 +0100

    refactor delete old lambda command

--- commands/awslambda.py
+++ commands/awslambda.py
@@ -1,30 +1,12 @@
 import click
-import boto3
+from commands.services.lambda_api import LambdaService
 
 @click.group()
 def awslambda():
     pass
 
-@awslambda.command(help="Clean old versions of AWS Lambda functions.")
+@awslambda.command(help="Delete old versions of AWS Lambda functions.")
 @click.option('--profile', envvar='AWS_PROFILE', default='default', help='The AWS profile to use.')
-@click.option('--delete', is_flag=True, help='Delete old versions.')
-def clean_old_versions(profile, delete):
-    session = boto3.Session(profile_name=profile)
-    client = session.client('lambda')
-
-    functions_paginator = client.get_paginator('list_functions')
-    version_paginator = client.get_paginator('list_versions_by_function')
-
-    for function_page in functions_paginator.paginate():
-        for function in function_page['Functions']:
-            aliases = client.list_aliases(FunctionName=function['FunctionArn'])
-            alias_versions = [alias['FunctionVersion'] for alias in aliases['Aliases']]
-            for version_page in version_paginator.paginate(FunctionName=function['FunctionArn']):
-                for version in version_page['Versions']:
-                    arn = version['FunctionArn']
-                    if version['Version'] != function['Version'] and version['Version'] not in alias_versions:
-                        print('  🥊 {}'.format(arn))
-                        if delete:
-                            client.delete_function(FunctionName=arn)
-                    else:
-                        print('  💚 {}'.format(arn))
+@click.option('--delete', is_flag=True, default=True, help='Delete old versions.')
+def delete_old_versions(profile, delete):
+    LambdaService(profile).delete_old_versions(delete)
